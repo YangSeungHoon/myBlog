44ea4283f2143f39660f614f0a9833fe86f4efdf
SENUNGHOONYANG <sheepsh93@gmail.com>
2021-12-17 13:57:42 +0900

    feat : Create entity User
    User Entity 생성.

---
+++ src/main/java/com/sheep/sh/myblog/model/User.java
@@ -0,0 +1,31 @@
+package com.sheep.sh.myblog.model;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length= 30)
+    private String username;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @ColumnDefault("'user'")
+    private String role;
+
+    @CreationTimestamp //시간 자동 입력
+    private Timestamp createDate;
+
+}
