27d59278a401c58d551058ae9711dc37107eeca1
SENUNGHOONYANG <sheepsh93@gmail.com>
2021-12-17 22:05:34 +0900

    feat : Create UserRepository
    UserRepository 생성.

---
+++ src/main/java/com/sheep/sh/myblog/UserRepository.java
@@ -0,0 +1,9 @@
+package com.sheep.sh.myblog;
+
+import com.sheep.sh.myblog.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
