a9be99f5636f60f0674830e2f08fefbf54f9ce6e
SENUNGHOONYANG <sheepsh93@gmail.com>
2021-12-19 16:51:29 +0900

    feat : Create user.js file
    1. ajax통신 코드 작성.
    2. 해당 코드는 회원가입과 로그인 관련 코드이다.

---
+++ src/main/resources/static/js/user.js
@@ -0,0 +1,49 @@
+let index = {
+    init: function (){
+        $("#btn-save").on("click",()=> {
+            this.save();
+        });
+    },
+
+    save:function(){
+        let data = {
+            username:$("#username").val(),
+            password:$("#password").val(),
+            email:$("#email").val()
+        };
+
+        $.ajax({
+            type:"POST",
+            url:"/blog/api/user",
+            data: JSON.stringify(data), //http boidy 데이터
+            contentType:"application/json; charset=utf-8", // body 데이터가 어떤 타입인지(MIME)
+            dataType:"json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열(생긴게 json) 이라면=> javascript object로 변경시켜준다.
+        }).done(function(res) {
+            location.href="/blog";
+        }).fail(function(err){
+            alert(JSON.stringify(err));
+        }); //ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청.
+    },
+
+
+    login:function(){
+        let data = {
+            username:$("#username").val(),
+            password:$("#password").val(),
+        };
+
+        $.ajax({
+            type:"POST",
+            url:"/blog/api/user/login",
+            data: JSON.stringify(data), //http boidy 데이터
+            contentType:"application/json; charset=utf-8", // body 데이터가 어떤 타입인지(MIME)
+            dataType:"json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열(생긴게 json) 이라면=> javascript object로 변경시켜준다.
+        }).done(function(res) {
+            location.href="/blog";
+        }).fail(function(err){
+            alert(JSON.stringify(err));
+        });
+    }
+}
+
+index.init();
