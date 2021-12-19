593b569e48b0c1116de8d0217da620d7390226cb
SENUNGHOONYANG <sheepsh93@gmail.com>
2021-12-19 17:40:49 +0900

    refactor : Change request address before using Spring Security.
    Spring Security 사용하기 전, 요청 주소를 변경한다.

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -17,12 +17,12 @@ let index = {
 
         $.ajax({
             type:"POST",
-            url:"/blog/api/user",
+            url:"/api/user",
             data: JSON.stringify(data), //http boidy 데이터
             contentType:"application/json; charset=utf-8", // body 데이터가 어떤 타입인지(MIME)
             dataType:"json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열(생긴게 json) 이라면=> javascript object로 변경시켜준다.
         }).done(function(res) {
-            location.href="/blog";
+            location.href="/";
         }).fail(function(err){
             alert(JSON.stringify(err));
         }); //ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청.
@@ -37,12 +37,12 @@ let index = {
 
         $.ajax({
             type:"POST",
-            url:"/blog/api/user/login",
+            url:"/api/user/login",
             data: JSON.stringify(data), //http boidy 데이터
             contentType:"application/json; charset=utf-8", // body 데이터가 어떤 타입인지(MIME)
             dataType:"json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열(생긴게 json) 이라면=> javascript object로 변경시켜준다.
         }).done(function(res) {
-            location.href="/blog";
+            location.href="/";
         }).fail(function(err){
             alert(JSON.stringify(err));
         });
